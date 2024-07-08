9546f3ad0e7e67189bec3f628ee729a4bb564bce
JosueGallegos <jgallegoss@est.ups.edu.ec>
2024-07-08 17:49:18 -0500

    Trabajp en clase

--- src/App.java
+++ src/App.java
@@ -8,25 +8,34 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Ejecuta el ejemplo de uso de HashMap con ejemplos sencillos
-        runMapExamlpe();
+        runMapExample();
 
         // Ejecuta el ejemplo de gestión de empleados usando HashMap
         runEmpleadoExample();
 
         // Ejecuta los ejercicios de sumatoria y anagramas
-        runEjerccios();
+        //runEjerccios();
     }
 
     private static void runEmpleadoExample() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        //throw new UnsupportedOperationException("Not implemented yet");
+        EmpleadoContoller empCtrl = new EmpleadoContoller();
+        empCtrl.addEmpleado(new Empleado(1, "Pablo", "Developer"));
+        empCtrl.addEmpleado(new Empleado(2, "Pablo", "Analyst"));
+        empCtrl.addEmpleado(new Empleado(3, "Miguel", "Developer"));
+        empCtrl.addEmpleado(new Empleado(400, "Mario", "Tester"));
+
+        empCtrl.imprimirEmpleados();
+
     }
 
-    private static void runMapExamlpe() {
-        throw new UnsupportedOperationException("Not implemented yet");
+    private static void runMapExample() {
+        //throw new UnsupportedOperationException("Not implemented yet");
+        new Mapa();
     }
 
     private static void runEjerccios() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        //throw new UnsupportedOperationException("Not implemented yet");
 
     }
 }
--- src/controllers/Mapa.java
+++ src/controllers/Mapa.java
@@ -4,5 +4,43 @@ import java.util.HashMap;
 
 public class Mapa {
     public Mapa() {
+        HashMap<String, Integer> mapita = new HashMap<>();
+        mapita.put("Manzanas", 20);
+        mapita.put("Pera", 25);
+        mapita.put("Naranja", 20);
+        mapita.put("Pera", 23);
+
+        System.out.println(mapita);
+        //Eliminar 
+        mapita.remove("Pera");
+        System.out.println(mapita);
+        //Para ver si han sido eliminadas y debuelve un false o true
+        System.out.println("Existe Mandariana -> " + mapita.containsKey("Mandarina"));
+        System.out.println("Existe Pera -> " + mapita.containsKey("Pera"));
+        
+        int valorManzana = mapita.get("Manzanas");
+        System.out.println("Valor de Manzana es : " + valorManzana);
+
+        System.out.println(mapita.size());
+
+        System.out.println("Esta vacio ? " + mapita.isEmpty());
+
+        System.out.println( "\n Claves de mapas ");
+        for(String clave : mapita.keySet()){
+            System.out.println(" - " + clave);
+
+        }
+        System.out.println("\n Valores de mapas ");
+        for(Integer value : mapita.values()){
+            System.out.println(" - " + value);
+
+        }
+        System.out.println();
+        System.out.println(mapita);
+        mapita.replaceAll((k,v) -> v*2);
+        System.out.println(mapita);
+        mapita.clear();
+        System.out.println(mapita);
+
     }
 }
